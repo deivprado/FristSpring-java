8deecbedf7f468f2be4baa318d67ef39ae5eac6c
deivprado <pradodavi12@gmail.com>
2025-05-15 13:04:20 -0300

    alterando classe User

--- src/main/java/com/prado/frist_spring/domain/User.java
+++ src/main/java/com/prado/frist_spring/domain/User.java
@@ -10,25 +10,4 @@ import lombok.Setter;
 public class User {
     private String name;
     private String email;
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
